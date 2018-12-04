2584c9a941f6025689cd9f7217ea4a921b5dfa59
Ralf Pannemans <ralf.pannemans@sap.com>
2018-12-04 22:29:48 +0100

    Add IntStack example

---
+++ src/examples/IntStack.java
@@ -0,0 +1,32 @@
+package examples;
+
+public class IntStack {
+
+	public IntStack(int size) {
+
+	}
+
+	// show the top element
+	public int peek() // throws
+	{
+		return 0;
+	}
+
+	// return number of stored elements
+	public int size() {
+		return 0;
+	}
+
+	// store an element
+	public void push(int number) // throws
+	{
+		return;
+	}
+
+	// return and remove the last stored element
+	public int pop() // throws
+	{
+		return 0;
+	}
+
+}
